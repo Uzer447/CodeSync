aa464b2d06b761a05d2bc96bc61e3db011443190
Uzer <mohduzer447@gmail.com>
2025-05-21 00:55:38 +0530

    initial commit

---
+++ src/components/providers/ConvexClerkProvider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ClerkProvider } from "@clerk/nextjs";
+import { useAuth } from "@clerk/nextjs";
+
+const convex = new ConvexReactClient(
+  process.env.NEXT_PUBLIC_CONVEX_URL!,
+);
+
+function ConvexClerkProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+}
+
+export default ConvexClerkProvider;
